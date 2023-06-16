8eb0ce9bea492ea09b384291c02c14f1aa35f5ad
cantnip <borhama302@gmail.com>
2023-06-16 15:22:34 -0700

    ..

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,10 @@
 
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
-printf("size of long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
-printf("size of float: %lu bytes(s)\n", (unsigned long)sizeof(f));
+printf("size of a char: %lu byte(s)\n", sizeof(char));
+printf("size of an int: %lu byte(s)\n", sizeof(int));
+printf("size of a long int: %lu bytes(s)\n", sizeof(long int));
+printf("size of long long int: %lu bytes(s)\n", sizeof(long long int));
+printf("size of float: %lu bytes(s)\n", sizeof(float));
 return (0);
 }
