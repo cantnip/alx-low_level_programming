8dd803da81e9bae9234befbb7e0f3ee4c6bf1246
cantnip <borhama302@gmail.com>
2023-06-17 12:39:21 -0700

    ..

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+* Description: Printz from a to z
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
