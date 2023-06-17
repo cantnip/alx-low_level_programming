4e921c674dbd8528799be225b5c5b32f119be1d2
cantnip <borhama302@gmail.com>
2023-06-17 12:21:12 -0700

    ..

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
 * main - entry point
 * Description: Printz from a to z
 * Return: 0
 */
-
-
-	int main(void)
+int main(void)
 
 {
 	for (int i = 0; i < 26; i++)
-		printf("%c", 'a' + i);
+		printf("%c", "a" + i);
 	int i;
 
 	for (i = 0; i < 26; i++)
