7e71b932460db8c58ca49528cc0fa2e3ea35e751
cantnip <borhama302@gmail.com>
2023-06-17 10:20:05 -0700

    ..

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,12 +3,17 @@
 /**
  * main - prints the alphabetic
  * RETURN: Always (Sucess)
+ *
  */
+
 int main(void)
 {
-	char c;
-	for (c = 'a'; c <= 'z'; c++)
-	putchar(c);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++
+	{
+	putchar(letter);
+	}
 	putchar('\n');
 	return (0);
 }
