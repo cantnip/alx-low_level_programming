cfe84a43fd3cdd10f5064071c5fba1f2169d7962
cantnip <borhama302@gmail.com>
2023-06-17 11:42:19 -0700

    ....

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet.
- *
- * Return: Always 0 (Success)
- */
+* main - entry point
+* Description: Printz from a to z
+* Return: 0
+*/
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-
 	int i;
-	
+
 	for (i = 0; i < 26; i++)
-	{
-		putchar(alp[i]);
-	}
-	putchar('\n');
+		printf("%c", 'a' + i);
+
+	printf("\n");
+
 	return (0);
 }
-
