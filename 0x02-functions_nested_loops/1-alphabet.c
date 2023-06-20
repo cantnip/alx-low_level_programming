f42be420fc7277e2511b61a5c65a6a5dda6456b6
cantnip <borhama302@gmail.com>
2023-06-20 14:42:39 -0700

    ..

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "mai.h"
+
+/**
+ * main - Entry point
+ * Description:"print_alphabet - make the alphabet"
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n'
+			)
+}
