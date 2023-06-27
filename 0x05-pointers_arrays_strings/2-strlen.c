467815ee21fe3fcfa4dae3fbc2e4a17a93fb230c
cantnip <borhama302@gmail.com>
2023-06-27 16:32:42 -0700

    ..

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,26 +1,14 @@
 /**
-* _atoi - converts sting to int
-* @s: string
-* Return: int value of the string
+ * _strlen - returns str length
+ * @s: string (char[])
+ * Return: length of string
 */
 
-int _atoi(char *s)
+int _strlen(char *s)
 {
-	unsigned int result = 0;
-	int sign = 1, i = 0;
+	int c = 0;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] == '-')
-			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		else if (result > 0)
-			break;
-		i++;
-	}
-	return (sign * result);
+	while (s[c] != '\0')
+		c++;
+	return (c);
 }
