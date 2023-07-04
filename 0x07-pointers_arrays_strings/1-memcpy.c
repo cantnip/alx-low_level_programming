f72dfffe24265316d29bf0bfd9ae3dbfd1e59dee
cantnip <borhama302@gmail.com>
2023-07-04 13:56:29 -0700

    ..

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,12 +5,10 @@
 * @dest: dest ptr
 * Return: pointer to dest
 */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		dest[i] = src[i];
-
-	
+	dest[i] = src[i];
+}
